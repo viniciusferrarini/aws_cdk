d0210adc6e10d54c8e89f486972f669f58cddf37
Vinicius Ferrarini <viniciusferrarini@outlook.com>
2021-03-02 11:15:14 -0300

    Aulas do curso

--- src/main/java/com/myorg/VpcStack.java
+++ src/main/java/com/myorg/VpcStack.java
@@ -1,15 +1,9 @@
 package com.myorg;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
 import org.jetbrains.annotations.Nullable;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.ec2.SubnetConfiguration;
-import software.amazon.awscdk.services.ec2.SubnetType;
 import software.amazon.awscdk.services.ec2.Vpc;
-import software.amazon.awscdk.services.ec2.Vpc.Builder;
 import software.constructs.Construct;
 
 public class VpcStack extends Stack {
